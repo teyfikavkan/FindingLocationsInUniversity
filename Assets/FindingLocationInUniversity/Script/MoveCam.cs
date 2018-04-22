8f7b07cae981688e06e56bfde011a44fae3f8754
Teyfik <mteyfikavkan@github.com>
2018-04-22 16:25:08 +0300

    Firebase Connection & Connecting Scenes
    +firebase connection has been done to get location value or set default location value in firebase
    +2 scenes were connected each other.
    +some object's positions have been changed to exit after reaching target location point.

---
+++ Assets/FindingLocationInUniversity/Script/MoveCam.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Unity.Editor;
+using Firebase.Database;
+
+public class MoveCam : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    private string tempHasLocation;
+
+    private int startCnt;
+
+
+    private Quaternion startRotation;
+    private Quaternion endRotation;
+    private DatabaseReference db;
+
+
+    //PlayerPrefs.GetString("PlayerName").ToString();
+    //db.SetValueAsync("nolocation");
+
+    void Start()
+    {
+        tempHasLocation = "nolocation";
+        startCnt = 0;
+
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://robotic-speech.firebaseio.com/");
+        db = FirebaseDatabase.DefaultInstance.GetReference("location");
+        
+
+        
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        startRotation = startMarker.rotation;
+        endRotation = Quaternion.Euler(0,0,0);
+
+
+    }
+
+    void Update()
+    {
+        try
+        {
+            if(startCnt==0)
+                    {
+                        hasLocation();
+                        if (!tempHasLocation.Equals("nolocation"))
+                        {
+                            startTime = Time.time;
+                            startCnt = 1;
+                        }
+            
+                    }
+            else
+            {
+                float distCovered = (Time.time - startTime) * speed;
+                float fracJourney = distCovered / journeyLength;
+                transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+                startMarker.position = transform.position;
+                transform.rotation = Quaternion.Lerp(startRotation, endRotation, distCovered*11);
+                startMarker.rotation = transform.rotation;
+                if(startMarker.position==endMarker.position)
+                {
+                    LoadMenu();
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("Update Method Exception :" + ex);
+        }
+        
+        
+
+    }
+
+
+    void hasLocation()
+    {
+   
+        
+        try
+        {
+
+            db.GetValueAsync().ContinueWith(task => {
+
+                if (task.IsFaulted)
+                {
+                    // Handle the error...
+                }
+                else if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    tempHasLocation = snapshot.GetValue(true).ToString();
+                    //Debug.Log(snapshot);
+                    //Debug.Log(tempHasLocation);
+
+                }
+            });
+
+           
+
+        }
+        catch(Exception ex)
+        {
+            Debug.Log("hasLocation Method Exception : "+ex);
+            tempHasLocation = "None";
+        }
+
+        
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("UniversityInside", LoadSceneMode.Single);
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.SetString("LocationID", tempHasLocation);
+    }
+
+    public void greatingMessage()
+    {
+        //TODO: animasyon eklenecek
+
+    }
+
+
+
+
+}
---
+++ Assets/FindingLocationInUniversity/Script/NavMeshMove.cs
@@ -0,0 +1,103 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class NavMeshMove : MonoBehaviour {
+
+    [SerializeField]
+
+    Transform _destination ;
+    GameObject _location;
+    NavMeshAgent _navMeshAgent;
+
+    DatabaseReference db;
+
+    // Use this for initialization
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://robotic-speech.firebaseio.com/");
+        db = FirebaseDatabase.DefaultInstance.GetReference("location");
+
+
+        string location=PlayerPrefs.GetString("LocationID").ToString();
+        _location = GameObject.Find(location);
+        _destination = _location.transform;
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("Erorrrr");
+        }
+
+        else
+        {
+            startInformation();
+            SetDestination();
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Target :"+_destination.transform.position);
+        Debug.Log("Start :" + _navMeshAgent.transform.position);
+        Transform _startdestination = _navMeshAgent.transform;
+        if (_navMeshAgent.transform.position.x ==_destination.transform.position.x)
+        {
+           
+            if (_navMeshAgent.transform.position.z == _destination.transform.position.z)
+            {
+               
+                if (_navMeshAgent.transform.position.y == _destination.transform.position.y)
+                {
+                    endInformation();
+                    db.SetValueAsync("nolocation");
+                    LoadMenu();
+                }
+            }
+           
+            
+            
+        }
+    }
+
+
+    private void SetDestination()
+    {
+        if (_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            //Debug.Log(targetVector);
+            
+            _navMeshAgent.SetDestination(targetVector);
+            
+
+        }
+        
+
+    }
+
+    public void LoadMenu()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("UniversityPerspective", LoadSceneMode.Single);
+        //Application.LoadLevel("UniversityPerspective");
+        
+    }
+
+    public void startInformation()
+    {
+        //TODO başlangıç için bilgi verecek
+    }
+
+    public void endInformation()
+    {
+        //TODO son için bilgi verecek
+    }
+}
