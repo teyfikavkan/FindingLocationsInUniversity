d4f1fd4fd3deb991e0b604595a83a877a85bf61b
Teyfik <mteyfikavkan@github.com>
2018-05-01 06:27:18 +0300

    Furniture for Labs
    +Some furnitures have been added and made as not walkable in navmesh

---
+++ Assets/FindingLocationInUniversity/Script/CameraCollision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour {
+	private Vector3 defaultCamLocalPos;
+	private float defaultCamDistance;
+
+	// Use this for initialization
+	void Start () {
+		defaultCamLocalPos = transform.localPosition;
+
+		Vector3 charPos = transform.parent.position;
+		Vector3 camPos = transform.position;
+		Vector3 charToCam = camPos - charPos;
+		defaultCamDistance = charToCam.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		RaycastHit hit;
+
+		Vector3 charPos = transform.parent.position;
+		Vector3 camPos = transform.position;
+		Vector3 charToCam = camPos - charPos;
+
+		//int mapLayer = LayerMask.NameToLayer("Map");
+		int mapLayerMask = LayerMask.GetMask(new string[] { "Map" });
+
+
+		float distance = defaultCamDistance;
+		
+		Debug.DrawRay(charPos, charToCam.normalized * distance, Color.magenta, 0, false);
+		
+		if (Physics.Raycast(charPos, charToCam.normalized, out hit, distance, mapLayerMask))
+		{
+			Debug.Log("camera hit " + hit.collider);
+			Vector3 newCamPos = hit.point + -0.5f * charToCam.normalized;
+			//Debug.DrawLine(charPos, newCamPos, Color.magenta, 0, false);
+			transform.position = newCamPos;
+		}
+		else
+		{
+			transform.localPosition = defaultCamLocalPos;
+		}
+	}
+}
